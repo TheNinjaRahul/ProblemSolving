ebd6288b2ef99776bc343fdb10f0a83e60577853
Rahul <chudasama.rahul.r@gmail.com>
2020-05-10 18:23:06 +0530

    adding Sudoku solution with short

---
+++ src/com/company/javaLogic/TestShort.java
@@ -0,0 +1,9 @@
+package com.company.javaLogic;
+
+public class TestShort {
+    public static void main(String[] args) {
+        short check = 0;
+        System.out.println((check << '5'-'1'));
+
+    }
+}
--- src/com/company/leetcode/array/ValidateSudoku.java
+++ src/com/company/leetcode/array/ValidateSudoku.java
@@ -19,9 +19,31 @@ public class ValidateSudoku {
                     {'.','.','.','.','8','.','.','7','9'}
             };
 
-        System.out.println(isValidSudoku(data));
+        System.out.println(isValidSudoku2(data));
     }
 
+    public static boolean isValidSudoku2(char[][] board) {
+        short[] rows = new short[9];
+        short[] cols = new short[9];
+        short[] squares = new short[9];
+
+        for(int row = 0; row < 9; row++) {
+            for(int col = 0; col < 9; col++) {
+                if(board[row][col] == '.') continue;
+
+                short value = (short) (1 << board[row][col] - '1');
+
+                if((value & rows[row]) > 0) return false;
+                if((value & cols[col]) > 0) return false;
+                if((value & squares[3*(row/3) + col/3]) > 0) return false;
+
+                rows[row] |= value;
+                cols[col] |= value;
+                squares[3*(row/3) + (col/3)] |= value;
+            }
+        }
+        return true;
+    }
     public static boolean isValidSudoku(char[][] board) {
 
 //        Set<Character>[] rowSet= HashSet<Character>()[3][9];
